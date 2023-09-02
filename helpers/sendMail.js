ef66ad705e4de4c8b6b35ff3098af8de1207afbf
Olena Hrechykhina <117777710+Elena-Hrechyk@users.noreply.github.com>
2023-09-02 11:46:29 -0700

    hw06-email

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -1,14 +1,14 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-const { META_PASSWORD } = process.env;
+const { META_PASSWORD, FROM_EMAIL } = process.env;
 
 const nodemailerConfig = {
   host: "smtp.meta.ua",
   port: 465,
   secure: true,
   auth: {
-    user: "olena_hrechykhina@meta.ua",
+    user: FROM_EMAIL,
     pass: META_PASSWORD,
   },
 };
@@ -18,7 +18,7 @@ const transpoter = nodemailer.createTransport(nodemailerConfig);
 const sendEmail = async (data) => {
   const email = {
     ...data,
-    from: "olena_hrechykhina@meta.ua",
+    from: FROM_EMAIL,
   };
   await transpoter.sendMail(email);
   return true;
